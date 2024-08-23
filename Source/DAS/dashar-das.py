0fe096f396a0e24beb6aaf08f9d32e2e4ea8c8e9
TrevorDBrown <trevordylanbrown@gmail.com>
2024-08-22 20:54:12 -0500

    Added basic Hello World server (Tornado).

--- Source/DAS/dashar-das.py
+++ Source/DAS/dashar-das.py
@@ -11,27 +11,42 @@ import das_core.obdii_interpreter
 import datetime
 import sys
 import signal
+import asyncio
+import tornado
 
-def ctrl_c_pressed(sig, frame):
-    # Set the active loop flag to False.
-    global flagActiveLoop
-    flagActiveLoop = False
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world!")
 
-def main() -> None:
+def make_app() -> None:
+    return tornado.web.Application([
+        (r"/", MainHandler)
+    ])
+
+# def ctrl_c_pressed(sig, frame):
+#     # Set the active loop flag to False.
+#     global flagActiveLoop
+#     flagActiveLoop = False
+
+async def main() -> None:
 
     # Initialization
     global flagActiveLoop
     flagActiveLoop = True
 
     # Register CTRL+C signal.
-    signal.signal(signal.SIGINT, ctrl_c_pressed)
+    #signal.signal(signal.SIGINT, ctrl_c_pressed)
 
     dasharOBDIIObject = das_core.obdii_interpreter.OBDIIContext()
 
+    app = make_app()
+    app.listen(3000)
+    await asyncio.Event().wait()
+
     # Main Loop
-    while (flagActiveLoop):
-        # print(dasharOBDIIObject, end="\033[F\033[A\033[A\033[A\033[A\033[A")
-        print(f"Date Fetched (Epoch): {datetime.datetime.now(tz=datetime.timezone.utc).timestamp()}", end="\033[F\033[A")
+    # while (flagActiveLoop):
+    #     # print(dasharOBDIIObject, end="\033[F\033[A\033[A\033[A\033[A\033[A")
+    #     print(f"Date Fetched (Epoch): {datetime.datetime.now(tz=datetime.timezone.utc).timestamp()}", end="\033[F\033[A")
 
 if (__name__ == "__main__"):
-    main()
+    asyncio.run(main())
