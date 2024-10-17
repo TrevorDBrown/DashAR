a0c81771d7579d2c98fe84baa88fae7d16f19a0a
TrevorDBrown <trevordylanbrown@gmail.com>
2024-10-16 23:15:22 -0500

    Various changes
    - Added async support for HTTP calls.
    - Fixed some bugs with the class setup.
    - Replaced System.Text.Json with Newtonsoft.Json (Unity had a problem with System.Text.Json)

--- Source/HUD/Assets/DashAR/Objects/DashARDataAggregatorServer.cs
+++ Source/HUD/Assets/DashAR/Objects/DashARDataAggregatorServer.cs
@@ -10,42 +10,53 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text.Json;
+using System.Threading.Tasks;
 
-public class DashARDataAggregatorServer
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class DashARDataAggregatorServer : MonoBehaviour
 {
     private Guid _id;
-    private readonly HttpClient _httpClient;
+    private HttpClient _httpClient;
 
     public DashARDataAggregatorServer ()
     {
         this._id = Guid.NewGuid();
         this._httpClient = new HttpClient();
+
+        this._httpClient.BaseAddress = new Uri("http://localhost:3000/");
     }
 
-    public DashARDataAggregatorServerResponse GetUpdateFromServer()
+    public async Task<DashARDataAggregatorServerResponse> GetUpdateFromServerAsync()
     {
         this._httpClient.DefaultRequestHeaders.Accept.Clear();
         this._httpClient.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("text/json")
+            new MediaTypeWithQualityHeaderValue("application/json")
         );
         this._httpClient.DefaultRequestHeaders.Add("User-Agent", "DashAR HUD");
 
-        string responseJsonAsString = ""; //TODO: figure this out... -> await this.GetUpdateFromServerAsync(this._httpClient);
+        string responseInJson = await this.GetAsync("/data/obdii");
 
-        return new DashARDataAggregatorServerResponse(responseJsonAsString);
+        Debug.Log("Response: " + responseInJson);
+        
+        DashARDataAggregatorServerResponse response = JsonConvert.DeserializeObject<DashARDataAggregatorServerResponse>(responseInJson);
 
+        return response;
     }
 
-    // private async Task GetUpdateFromServerAsync(HttpClient client){
-    //     string response = await client.GetStringAsync(
-    //         "http://127.0.0.1:3000/data/obdii"
-    //     );
-
-    //     return response;
-    // }
-
-
-
+    public async Task<string> GetAsync(string uri)
+    {
+        HttpResponseMessage response = await this._httpClient.GetAsync(uri);
+        
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadAsStringAsync();
+        }
+        else
+        {
+            return "";
+        }
+    }
 
 }
