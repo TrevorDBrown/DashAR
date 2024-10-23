a2d3045d8d4545cac75dbeb11d41af1792ec5f26
TrevorDBrown <trevordylanbrown@gmail.com>
2024-10-22 23:21:03 -0500

    Update das_service.py
    - Removed single run option.
    - Added new endpoints for init and quit.
    - Renamed third party API endpoint.

--- Source/DAS/das_service.py
+++ Source/DAS/das_service.py
@@ -59,13 +59,49 @@ class OBDIIHandler(tornado.web.RequestHandler):
             self.write(f"{client_response_json}")
 
 class ThirdPartyAPIHandler(tornado.web.RequestHandler):
+    dashar_configuration: Configuration
+
+    def initialize(self, dashar_configuration: Configuration) -> None:
+        self.dashar_configuration = dashar_configuration
+
     def get(self) -> None:
-        self.write("This is a test!")
+        self.write("This endpoint is for third-party API calls.")
+
+class InitHandler(tornado.web.RequestHandler):
+    dashar_configuration: Configuration
+
+    def initialize(self, dashar_configuration: Configuration) -> None:
+        self.dashar_configuration = dashar_configuration
+
+    def get(self) -> None:
+        self.write("Initializing...")
+
+class TerminateHandler(tornado.web.RequestHandler):
+    dashar_configuration: Configuration
+
+    def initialize(self, dashar_configuration: Configuration) -> None:
+        self.dashar_configuration = dashar_configuration
+
+    def _terminate(self) -> None:
+        print("Termination signal received from /quit endpoint.")
+
+        # event_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+
+        # event_loop.stop()
+        # event_loop.close()
+
+        return
+
+    def get(self) -> None:
+        self.write("Termination signal received.")
+        self._terminate()
 
 def make_app(dashar_configuration: Configuration) -> tornado.web.Application:
     return tornado.web.Application([
+        (r"/init", InitHandler, {"dashar_configuration": dashar_configuration}),
+        (r"/quit", TerminateHandler, {"dashar_configuration": dashar_configuration}),
         (r"/data/obdii", OBDIIHandler, {"dashar_configuration": dashar_configuration}),
-        (r"/data/api/google-maps", ThirdPartyAPIHandler, {"dashar_configuration": dashar_configuration})
+        (r"/data/tpapi", ThirdPartyAPIHandler, {"dashar_configuration": dashar_configuration})
     ])
 
 def check_for_arguments():
@@ -73,7 +109,6 @@ def check_for_arguments():
     argument_parser = argparse.ArgumentParser(  prog='das_service.py',
                                                 description='The Data Aggregator and Server Component of the DashAR System.')
 
-    argument_parser.add_argument("-s", "--single-run", action="store_true", help="Flag indicating a single instance store in a SQLite Database.")
     argument_parser.add_argument("-v", "--verbose", action="store_true", help="Output additional information at runtime.")
     argument_parser.add_argument("--headless", action="store_true", help="Capture data points without external requests.")
 
@@ -87,14 +122,24 @@ async def main() -> None:
     # Initialization
     dashar_configuration: Configuration = Configuration(arguments)
 
+    http_port: int = 3832
+
     if (not dashar_configuration.configuration_variables.headless_operation):
 
         app: tornado.web.Application = make_app(dashar_configuration)
-        app.listen(3000)
+
+        print(f"\nSystem ready on port {http_port}.\n")
+
+        app.listen(http_port)
+        await asyncio.Event().wait()
         await asyncio.Event().wait()
 
     else:
-        print("DAS is running headless mode. Data will be captured over set time frequency.")
+
+        sleep_time: float = 0.5
+
+        print(f"DAS is running headless mode. Data will be captured over set time frequency (currently {sleep_time} seconds between calls).")
+
 
         while True:
             if (dashar_configuration.obdii_context.is_connected()):
@@ -103,12 +148,15 @@ async def main() -> None:
 
                 print(obdii_response)
 
-                # Wait half a second before trying again.
-                time.sleep(0.5)
+                # Wait specified time before trying again.
+                time.sleep(sleep_time)
 
             else:
                 print("OBDII is not connected. Exiting.")
                 break
+
+    print("System has terminated.")
+
 
 if (__name__ == "__main__"):
     asyncio.run(main())
