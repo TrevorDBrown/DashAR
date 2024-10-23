41b71df8b3a856297d27ef8b6ce20ef43193c5a8
TrevorDBrown <trevordylanbrown@gmail.com>
2024-10-22 23:22:03 -0500

    Update DashARStateMachine.cs
    - Minor changes to optimize file.
    - Added Clock widget to list of widgets.

--- Source/HUD/Assets/DashAR/Objects/DashARStateMachine.cs
+++ Source/HUD/Assets/DashAR/Objects/DashARStateMachine.cs
@@ -9,7 +9,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 using UnityEngine;
 
@@ -21,22 +20,30 @@ public class DashARStateMachine : MonoBehaviour
     private DashARHUDDevice _hudDevice;
     private DashARDataAggregatorServer _dasAPI;
 
-    // Gauge List
     private List<DashARGauge> _gauges;      // Built-in gauges include: Speedometer (N), Tachometer (N), Fuel Level (N), and Compass (Y).
 
     public DashARStateMachine ()
     {
         this._id = Guid.NewGuid();
+
+        // Contextualize the HUD Device.
+        this._hudDevice = new DashARHUDDevice();
+
+        // Establish the entry point for the HUD gauge objects,
         this._origin = GameObject.Find("HUD");
 
+        // Initialize the gauges.
+        // TODO: swap out for parsing of config files with gauge data.
         this._gauges = new List<DashARGauge>();
         this._gauges.Add(new DashARGauge(gaugeName: "Speedometer", gaugeValueType: "string", gaugeUnitOfMeasure: "mph", dataSource: "DAS", dataSourceMappedValue: "current_speed"));
         this._gauges.Add(new DashARGauge(gaugeName: "Compass", gaugeValueType: "string", gaugeUnitOfMeasure: "cardinal", dataSource: "HUD Device", suppressUnitOfMeasureOnDisplay: true));
+        this._gauges.Add(new DashARGauge(gaugeName: "Clock", gaugeValueType: "string", gaugeUnitOfMeasure: "time", dataSource: "HUD Device", suppressUnitOfMeasureOnDisplay: true));
 
-        this._hudDevice = new DashARHUDDevice();
-        
-        //string httpHost = "http://192.168.3.1:3000/";   // The IP address of the network self-hosted by the Raspberry Pi 5.
-        string httpHost = "http://localhost:3000/";     // The local machine (testing on development machine only...)
+        // NOTE: use these to override the host IP inference process.
+        //string httpHost = "192.168.2.1:3832";   // The IP address assigned by my MacBook Pro when sharing my loopback address.
+        string httpHost = "192.168.3.1:3832";   // The IP address of the network self-hosted by the Raspberry Pi 5.
+        //string httpHost = "localhost:3832";     // The local machine (testing on development machine only...)
+        //string httpHost = "";
 
         this._dasAPI = new DashARDataAggregatorServer(httpHost);
 
@@ -45,24 +52,25 @@ public class DashARStateMachine : MonoBehaviour
 
     public async void PollForUpdate()
     {
-        DashARDataAggregatorServerResponse responseFromServer = await this._dasAPI.GetUpdateFromServerAsync();
-
         foreach (DashARGauge currentGauge in this._gauges)
         {
-
             if (currentGauge.DataSource == "DAS")
             {
+                DashARDataAggregatorServerResponse responseFromServer = await this._dasAPI.GetUpdateFromServerAsync();
+
                 if (currentGauge.Name == "Speedometer")
                 {
-                    Debug.Log("Updating speed...");
                     currentGauge.UpdateGauge(responseFromServer.obdii_data.speed);
                 }
             } else if (currentGauge.DataSource == "HUD Device") {
                 if (currentGauge.Name == "Compass")
                 {
-                    Debug.Log("Updating compass...");
                     currentGauge.UpdateGauge(this._hudDevice.GetCompassHeading());
 
+                }
+                else if (currentGauge.Name == "Clock")
+                {
+                    currentGauge.UpdateGauge(this._hudDevice.GetCurrentTime());
                 }
             }
 
