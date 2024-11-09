d14d4d640ceb06eeb8922bf655d5db54ec48536e
TrevorDBrown <trevordylanbrown@gmail.com>
2024-11-08 23:27:32 -0600

    Create TestExtension.py
    - Created a test extension for the system.
    - TODO: implement basic functionality, and utilize importlib to make calls to extension scripts.

---
+++ Source/DAS/extensions/TestExtension/TestExtension.py
@@ -0,0 +1,19 @@
+#
+#   DashAR - An AR-based HUD for Automobiles.
+#   (c)2024 Trevor D. Brown. All rights reserved.
+#   This project is distributed under the MIT license.
+#
+#   File:       TestExtension.py
+#   Purpose:    This is a test extension for use in the DashAR system.
+#
+
+class TestExtension:
+    def __init__(self) -> None:
+        return
+
+    def main() -> None:
+        print("This is a text extension!")
+        return
+
+if (__name__ == "__main__"):
+    print("This extension should be used as an import for the DashAR System.")
