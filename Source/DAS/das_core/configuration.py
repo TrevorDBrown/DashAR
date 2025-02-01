2ce19ad277bdce22494394c9195ab7a431fbc297
TrevorDBrown <trevordylanbrown@gmail.com>
2025-01-31 23:04:55 -0600

    Update configuration.py
    - Fixed copyright information.
    - Removed unnecessary versioning information.
    - Began adding support for extensions.
    - Began adding support for server-side provisioning of HUD configurations to HUD app.
    - Began adding support for storing additional vehicle information (e.g. year, make, model, and mileage.)

--- Source/DAS/das_core/configuration.py
+++ Source/DAS/das_core/configuration.py
@@ -1,7 +1,6 @@
 #
 #   DashAR - An AR-based HUD for Automobiles.
-#   (c)2024 Trevor D. Brown. All rights reserved.
-#   This project is distributed under the MIT license.
+#   (c)2024-2025 Trevor D. Brown. Distributed under the MIT license.
 #
 #   File:       configuration.py
 #   Purpose:    This script contains the DashAR configuration shared amongst multiple modules.
@@ -12,15 +11,13 @@ import os
 import json
 import argparse
 
-from das_core.helper import Constants, SharedFunctions, ServiceMode
+from das_core.helper import Constants, SharedFunctions, ServiceMode, SystemStatus
 from das_core.obdii_interpreter import OBDIIContext
+from das_core.das_extensions import DASExtensions
 
 class ConfigurationConstants():
     CONFIGURATION_VERSION: str
     DASHAR_VERSION: str
-    DAS_VERSION: str
-    HUD_VERSION: str
-    COMPANION_APP_VERSION: str
 
     CONFIGURATION_PATH: Final[str] = os.path.join(os.getcwd(), "config.json")
 
@@ -28,15 +25,21 @@ class ConfigurationConstants():
         return
 
 class ConfigurationVariables():
+    system_status: SystemStatus = SystemStatus.NOT_STARTED
     das_server_port: int = 8000
     fuel_level_refresh_frequency_data_points: int = 200
-    data_path: str = os.path.join(os.getcwd(), "data")
-    database_path: str = os.path.join(data_path, "dashar-data.sqlite3")
     service_mode: ServiceMode
-    headless_operation: bool = False
     verbose_operation: bool = False
 
     obdii_elm327_device_path: str
+
+    data_path: str = os.path.join(os.getcwd(), "data")
+    database_path: str = os.path.join(data_path, "dashar-data.sqlite3")
+
+    das_extensions_path: str = os.path.join(os.getcwd(), "extensions")
+    das_extensions: DASExtensions = DASExtensions()
+
+    hud_configuration: dict = {}
 
     def __init__(self) -> None:
         return
@@ -54,7 +57,7 @@ class Configuration():
 
         # # Splash Message.
         print(f"DashAR Automotive HUD System")
-        print(f"(c)2024 Trevor D. Brown. All rights reserved.\n")
+        print(f"(c)2024-2025 Trevor D. Brown. Distributed under the MIT license.\n")
 
         # Set defaults ahead of time.
         self.set_default_configuration()
@@ -63,23 +66,20 @@ class Configuration():
         self.load_configuration(arguments)
 
         # Print remaining splash screen information.
-        print(f"DashAR System version {self.configuration_constants.DASHAR_VERSION}")
+        print(f"\nDashAR System version {self.configuration_constants.DASHAR_VERSION}")
         print(f"Configuration version {self.configuration_constants.CONFIGURATION_VERSION}")
-        print(f"Data Aggregator and Server (DAS) version {self.configuration_constants.DAS_VERSION}")
-        print(f"HUD App version {self.configuration_constants.HUD_VERSION}")
-        print(f"Companion App version {self.configuration_constants.COMPANION_APP_VERSION}")
+
+        if (self.configuration_variables.system_status == SystemStatus.FAILED):
+            print("\nError: configuration not loaded.")
+
+        else:
+            self.configuration_variables.system_status = SystemStatus.READY
 
         return
 
     def load_configuration(self, arguments: argparse.Namespace) -> None:
 
         # Check the arguments passed in.
-        # Headless Mode - automatic polling of data on set interval.
-        if (arguments.headless):
-            self.configuration_variables.headless_operation = True
-        else:
-            self.configuration_variables.headless_operation = False
-
         # Verbose Output - output all information.
         if (arguments.verbose):
             self.configuration_variables.verbose_operation = True
@@ -92,32 +92,26 @@ class Configuration():
         with open(self.configuration_constants.CONFIGURATION_PATH, "r") as json_configuration:
             json_configuration_content = json.load(json_configuration)
 
-        if (not json_configuration_content["config_version"]):
+        if (not json_configuration_content["versioning"]["configuration"]):
             print("No configuration file found. Using defaults.")
 
         else:
-            if (json_configuration_content["config_version"]):
-                self.configuration_constants.CONFIGURATION_VERSION = json_configuration_content["config_version"]
-
             if (json_configuration_content["versioning"]):
                 try:
                     self.configuration_constants.DASHAR_VERSION = json_configuration_content["versioning"]["dashar"]
-                    self.configuration_constants.DAS_VERSION = json_configuration_content["versioning"]["das"]
-                    self.configuration_constants.HUD_VERSION = json_configuration_content["versioning"]["hud"]
-                    self.configuration_constants.COMPANION_APP_VERSION = json_configuration_content["versioning"]["companion"]
+                    self.configuration_constants.CONFIGURATION_VERSION = json_configuration_content["versioning"]["configuration"]
 
                 except Exception as e:
                     print("Error: invalid versioning provided.")
                     self.configuration_constants.DASHAR_VERSION = "-1"
-                    self.configuration_constants.DAS_VERSION = "-1"
-                    self.configuration_constants.HUD_VERSION = "-1"
-                    self.configuration_constants.COMPANION_APP_VERSION = "-1"
+                    self.configuration_constants.CONFIGURATION_VERSION = "-1"
 
             if (json_configuration_content["service_mode"]):
                 try:
                     self.configuration_variables.service_mode = ServiceMode[str(json_configuration_content["service_mode"]).upper()]
                 except Exception as e:
-                    print(f"Error: Service Mode '{json_configuration_content['service_mode']}' is not a valid service mode.")
+                    print(f"Error: Service Mode '{json_configuration_content['service_mode']}' is not a valid service mode. Setting to default mode.")
+                    self.configuration_variables.service_mode = ServiceMode.TEST
             else:
                 self.configuration_variables.service_mode = ServiceMode["TEST"]
 
@@ -136,6 +130,13 @@ class Configuration():
                     database_path_content = database_path_content.replace("{DATA_PATH}", self.configuration_variables.data_path)
 
                 self.configuration_variables.database_path = os.path.abspath(database_path_content)
+
+            if (json_configuration_content["extensions"]):
+                for extension in json_configuration_content["extensions"]:
+                    self.configuration_variables.das_extensions.register_extension(name=extension["name"], description=extension["description"], path=extension["path"], module=extension["module"], functions=extension["functions"])
+
+                # TODO: remove me when done!!!
+                print(self.configuration_variables.das_extensions)
 
             if (json_configuration_content["variables"]):
                 for configuration_variable in json_configuration_content["variables"]:
@@ -159,11 +160,19 @@ class Configuration():
         valid_configuration: bool = self.test_configuration()
 
         if (not valid_configuration):
-            print("Error: invalid configuration provided. Resetting all values to defaults.")
+            print("\nError: invalid configuration provided. Resetting all values to defaults.")
             self.set_default_configuration()
 
         # Initialize the OBDII context.
         self.obdii_context = OBDIIContext(obdii_interface_device_path=self.configuration_variables.obdii_elm327_device_path, database_path=self.configuration_variables.database_path, service_mode=self.configuration_variables.service_mode)
+
+        if ((not self.obdii_context.is_connected())):
+            self.configuration_variables.service_mode = ServiceMode.INVALID
+            self.configuration_variables.system_status = SystemStatus.FAILED
+            return
+
+        # Configuration is good, and the OBDII connection is established.
+        self.configuration_variables.system_status = SystemStatus.READY
 
         return
 
@@ -171,12 +180,10 @@ class Configuration():
         # Set default configuration, to be overwritten by the JSON config load process.
         self.configuration_constants.CONFIGURATION_VERSION = Constants.EXPECTED_CONFIGURATION_VERSION
         self.configuration_constants.DASHAR_VERSION = Constants.EXPECTED_DASHAR_VERSION
-        self.configuration_constants.DAS_VERSION = Constants.EXPECTED_DAS_VERSION
-        self.configuration_constants.HUD_VERSION = Constants.EXPECTED_HUD_VERSION
-        self.configuration_constants.COMPANION_APP_VERSION = Constants.EXPECTED_COMPANION_APP_VERSION
+        self.configuration_variables.system_status = SystemStatus.STARTING
         self.configuration_variables.fuel_level_refresh_frequency_data_points = 200
         self.configuration_variables.service_mode = ServiceMode.TEST
-        self.configuration_variables.obdii_elm327_device_path = "/dev/tty.usbserial-D395GRKM"
+        self.configuration_variables.obdii_elm327_device_path = "COM4"
 
     def test_configuration(self) -> bool:
         error_count: int = 0
@@ -191,7 +198,7 @@ class Configuration():
         if (self.configuration_constants.CONFIGURATION_VERSION == Constants.EXPECTED_CONFIGURATION_VERSION):
             print(f"Configuration Version ({self.configuration_constants.CONFIGURATION_VERSION}) is valid.")
         else:
-            print(f"Warning: configuration version of file ({self.configuration_constants.CONFIGURATION_VERSION}) is older than the expected configuration version ({Constants.EXPECTED_CONFIGURATION_VERSION}). Please review configuration specification for changes.")
+            print(f"Warning: configuration version of file ({self.configuration_constants.CONFIGURATION_VERSION}) differs from the expected configuration version ({Constants.EXPECTED_CONFIGURATION_VERSION}). Please review configuration specification for changes.")
             warning_count += 1
 
         # Data Path Test
