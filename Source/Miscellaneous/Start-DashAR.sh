a27c3f9485d78ec595c0fa4f058231a2e70e4ffa
TrevorDBrown <trevordylanbrown@gmail.com>
2025-03-30 21:54:29 -0500

    Update Start-DashAR.sh
    - Added support for fetching the script directory, and in turn finding the project directory.
    - Adjusted placeholder values.

--- Source/Miscellaneous/Start-DashAR.sh
+++ Source/Miscellaneous/Start-DashAR.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+#
+#   DashAR - An AR-based HUD for Automobiles.
+#   (c)2024-2025 Trevor D. Brown. Distributed under the MIT license.
+#
+#   File:       Start-DashAR.sh
+#   Purpose:    This script starts DAS API for the DashAR System.
+#
+
+# Set up variables
+SCRIPT_PATH=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+cd $SCRIPT_PATH
+cd "../../"
+PROJECT_ROOT=$PWD
+
 # Replace the following with the path to the DashAR Project.
-cd "REPLACEME-DASHAR/DashAR/Source/DAS/"
+cd $PROJECT_ROOT/DashAR/Source/DAS/
 
 # Replace the following with the path to your Python virtual environment and the path to the DashAR project, respectively.
-"[REPLACEME-PYVENV]/bin/python3" [REPLACEME-DASHAR]/DashAR/Source/DAS/das_service.py
+"[REPLACE ME WITH PATH TO VENV]/bin/python3" $PROJECT_ROOT/Source/DAS/das_service.py
